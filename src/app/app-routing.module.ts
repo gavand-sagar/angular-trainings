87b7716e6f15afb4f0a8cfd50d1b6e1f3247c1ee
gavand-sagar <gavand.sagar.s@gmail.com>
2022-04-20 07:29:32 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'products', component: ProductsComponent },
+
+  { path: 'popular-path', redirectTo: 'products' }
+  // { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RatingComponent } from './rating/rating.component';
-import { AddSongComponent } from './song-add-form/song-add-form.component';
-import { SongItemComponent } from './song-item/song-item.component';
-import { SortButtonComponent } from './sort-button/sort-button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SortButtonComponent,
-    SongItemComponent,
-    RatingComponent,
-    AddSongComponent
   ],
   imports: [
     BrowserModule,
